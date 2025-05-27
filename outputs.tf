256225cf7b52603b08ffb42b54949be5177f6050
Nikhil baghel <nikhil.baghel@clouddrove.com>
2025-05-27 17:01:27 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,16 @@
-# ------------------------------------------------------------------------------
-# Outputs
-# ------------------------------------------------------------------------------
-output "label_order" {
-  value       = local.label_order
-  description = "Label order."
+output "nat_instance_public_ip" {
+  value       = aws_instance.nat_instance.public_ip
+  description = "Public IP of the NAT instance"
+}
+
+output "vpc_id" {
+  value = aws_vpc.nat_vpc.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
 }
